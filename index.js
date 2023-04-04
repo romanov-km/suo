896e784c9c50fd1bfefd8d63953365d4f32eca7a
Kirill Romanov <102871501+romanov-km@users.noreply.github.com>
2023-04-05 02:00:13 +0300

    add fonts styles

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('123');
+
+const buttonMenu = document.querySelectorAll('.menu__item-1');
+
+
+
+buttonMenu.addEventListener('click', function (event) {
+    console.log('Произошло событие', event.type)
+});
+
+
+const element = document.querySelector('button')
+
+element.addEventListener('click', function (event) {
+  console.log('Произошло событие', event.type)
+})
